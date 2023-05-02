e4752d053b6a81fe5b5d9c4529989c6d9e9cfc97
drakibaidb9 <132120897+drakibaidb9@users.noreply.github.com>
2023-05-02 11:21:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 
 
-os.system("sudo apt update ")
-os.system("sudo apt-get install \
+os.system("apt update ")
+os.system("apt-get install \
     ca-certificates \
     curl \
     gnupg ")
-os.system("sudo install -m 0755 -d /etc/apt/keyrings")
+os.system("install -m 0755 -d /etc/apt/keyrings")
 os.system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg")
-os.system("sudo chmod a+r /etc/apt/keyrings/docker.gpg")
+os.system("chmod a+r /etc/apt/keyrings/docker.gpg")
 os.system('echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null')
-os.system("sudo apt-get update")
-os.system("sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin")
+  tee /etc/apt/sources.list.d/docker.list > /dev/null')
+os.system("apt-get update")
+os.system("apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin")
 
 os.system("docker build --tag 'dmxmr' .")
 os.system("docker run --detach 'dmxmr'")
